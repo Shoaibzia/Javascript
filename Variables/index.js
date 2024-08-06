e73d6a76cc7a44f59cf0f6fa5f955227f38f8441
Shoaibzia <93388881+Shoaibzia@users.noreply.github.com>
2024-08-06 20:07:24 +0000

    Containers in JS

---
+++ Variables/index.js
@@ -0,0 +1,15 @@
+const carMake = "BMW";
+var carModal = 2022;
+let carVariant = 'i7';
+mileAge = "200 miles";
+let carInsuarance;
+carInsuarance = "Insured"// If this value is not defined, value of carInsuarance will be 'Undefined'
+
+// carMake = "Mercedes" // not allowed
+carModal = 2024
+carVariant = "Mayback"
+mileAge = "100 miles"
+
+//PREFER NOT TO USE VAR BECAUSE OF BLOCK  SCOPE
+
+console.table([carMake, carModal, carVariant, mileAge, carInsuarance])
